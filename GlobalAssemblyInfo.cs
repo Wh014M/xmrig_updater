dd7814dca75fdf221988b7631ca307a444b73228
lukacci <40762793+lukacci@users.noreply.github.com>
2019-03-13 13:55:19 +0000

    Add files via upload

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("master")]
+[assembly: AssemblyCompany("http://ramonsmits.com")]
+[assembly: AssemblyProduct("xmrigEncoder")]
+[assembly: AssemblyCopyright("Copyright © Ramon Smits 2012")]
+[assembly: AssemblyTrademark("")]
